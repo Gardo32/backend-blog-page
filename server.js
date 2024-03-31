c0f61014ee1057b1ce9751393b0d5a4a6c4caccd
Gardo32 <gardodaqqaq@gmail.com>
2024-03-31 08:28:51 +0000

    Part 19

--- server.js
+++ server.js
@@ -1,18 +1,40 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import connectDB from "./config/db";
+import cors from "cors";
+import {
+  errorResponserHandler,
+  invalidPathHandler,
+} from "./middleware/errorHandler";
+
+// Routes
+import userRoutes from "./routes/userRoute.js";
+import postRoutes from "./routes/postRoutes.js";
+import commentRoutes from "./routes/commentRoutes";
+import postCategoriesRoutes from "./routes/postCategoriesRoutes";
 
 dotenv.config();
-
+connectDB();
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Server is ready");
+  res.send("Server is running...");
 });
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/post-categories", postCategoriesRoutes);
+
+// static assets
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+app.use(invalidPathHandler);
+app.use(errorResponserHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running at https://potential-space-enigma-9r697jx96w9fv7p-${PORT}.app.github.dev`);
-    });
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
